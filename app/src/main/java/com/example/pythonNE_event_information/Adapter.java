0928024393c81af27cf2f79bce691efc8ad3a0b8
kérous <88147887+armentanoc@users.noreply.github.com>
2023-09-20 17:03:04 -0300

    Add onclick listener and toast to open event URL

--- app/src/main/java/com/example/pythonNE_event_information/Adapter.java
+++ app/src/main/java/com/example/pythonNE_event_information/Adapter.java
@@ -1,7 +1,9 @@
 package com.example.pythonNE_event_information;
 
 import android.content.Context;
+import android.text.Html;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -11,12 +13,14 @@ import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<ViewHolder> {
 
-    Context context;
-    List<Item> items;
+    private Context context;
+    private List<Item> items;
+    private SelectListener listener;
 
-    public Adapter(Context context, List<Item> items) {
+    public Adapter(Context context, List<Item> items, SelectListener listener) {
         this.context = context;
         this.items = items;
+        this.listener = listener;
     }
 
     //Layout Inflater
@@ -29,18 +33,24 @@ public class Adapter extends RecyclerView.Adapter<ViewHolder> {
     //Bind the data to the view
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.titleView.setText(items.get(position).getTitle());
+        String text = "<a href='"+items.get(position).getUrl()+"'>"+items.get(position).getTitle()+"</a>";
+        holder.titleView.setText(Html.fromHtml(text));
         holder.typeView.setText(items.get(position).getType());
         holder.startView.setText(items.get(position).getStart());
         holder.durationView.setText(items.get(position).getDuration());
         holder.dateView.setText(items.get(position).getDateStr());
         holder.languageView.setText(items.get(position).getLanguage());
         holder.strRoomView.setText(items.get(position).getStrRoom());
-        holder.urlView.setText(items.get(position).getUrl());
         holder.abstractView.setText(items.get(position).getStrAbstract());
         holder.personNameView.setText(items.get(position).getPersonName());
-        //holder.imageView.setImageResource(items.get(position).getPlaceholder());
-        //holder.personBiographyView.setText(items.get(position).getPersonBiography());
+
+        //Listener para a URL
+        holder.titleView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClicked(items.get(position));
+            }
+        });
     }
 
     @Override
